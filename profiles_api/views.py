3bb608634cd8d2cc15303b6648dc2d0f11296794
jorgeayalah <ayalahjorge@gmail.com>
2024-03-21 16:13:33 -0600

    added profile viewset and permissions with auth token

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status #for using the HTTP codes
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 # When to use an APIView?
 # - Need full control over the logic
@@ -115,3 +117,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )    # Must be a tuple
+    permission_classes = (permissions.UpdateOwnProfile, )
