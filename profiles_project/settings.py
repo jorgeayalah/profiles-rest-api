0cd7c8dfa647c074ecee745d4ed8c4afd96ca41a
jorgeayalah <ayalahjorge@gmail.com>
2024-03-25 13:27:57 -0600

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_1b2zoztq&!oozn62)ino1u56cg!5ue9o@ypc)+c*6pc4o@*e@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 
 # Configure Django to use our custom user model instead of the default one
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+
+STATIC_ROOT = 'static/'
