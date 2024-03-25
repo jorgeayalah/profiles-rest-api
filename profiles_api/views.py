4c974f53cbece3a7a2cb3c6e16cbb2f805cfccbb
jorgeayalah <ayalahjorge@gmail.com>
2024-03-25 12:39:01 -0600

    added user profile feed API with permissions

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,9 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly #to prevent non owners to edit others items
+from rest_framework.permissions import IsAuthenticated #to prevent non registered users to see the app
+
 
 from profiles_api import serializers
 from profiles_api import models
@@ -136,8 +139,15 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication, )
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus, 
+        # IsAuthenticatedOrReadOnly,    # check imports
+        IsAuthenticated
+    )
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
         # the perform create function is a Django Rest F that allows us to override the behavior for creating objects through a ModelViewSet
         serializer.save(user_profile=self.request.user) # since its already authenticated
+
+
